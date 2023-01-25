073f9a0e3bc102f93886cb7af7b2ef9d070a4986
maxefimov <efimov.academy.top@gmail.com>
2023-01-25 21:26:37 +0300

    Добавьте файлы проекта.

---
+++ DPSolidIProject/DPSolidIProject.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class IMessage
+{
+    string text;
+    string toAddress;
+    string fromAddress;
+    string subject;
+
+public:
+    virtual string& Text() = 0;
+    string& ToAddress(){ return toAddress; };
+    string& FromAddress() { return fromAddress; };
+    string& Subject() { return subject; };
+
+    virtual void Send() = 0;
+};
+
+class EmailMessage : public IMessage
+{
+public:
+    void Send() override
+    {
+        cout << "From " << FromAddress <<
+            "To " << ToAddress <<
+            "Subject: " << Subject <<
+            "Message: " << Text << "\n";
+    }
+};
+
+class SmsMessage : public IMessage
+{
+    void Send() override
+    {
+        cout << "From " << FromAddress <<
+            "To " << ToAddress <<
+            "Message: " << Text << "\n";
+    }
+};
+
+int main()
+{
+    
+}
